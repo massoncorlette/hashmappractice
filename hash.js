71a6ff8a8bc510384be0c966e3d08989723dbc54
massoncorlette <massoncorlette07@gmail.com>
2024-09-27 04:02:40 -0400

    setting up functions, making sure that I am getting a hashcode that results in modulo return and is same code for same input

---
+++ hash.js
@@ -0,0 +1,75 @@
+
+
+const hashMap = function() {
+
+  let hashTable = [];
+  let currentCapacity = 16;
+  hashTable.length = currentCapacity;
+
+  function hash(key) {
+    let hashCode = 0;
+      
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+ 
+    return hashCode;
+  }
+
+  function set(key, value) {
+    const hashCode = hash(key) % currentCapacity;
+    console.log(hashCode);
+
+  }
+
+  function get(key) {
+
+  }
+
+  function has(key) {
+
+  }
+
+  function remove(key) {
+
+  }
+
+  function length() {
+
+  }
+
+  function clear() {
+
+  }
+
+  function keys() {
+
+  }
+
+  function values() {
+
+  }
+
+  function entries() {
+
+  }
+
+  return {
+    set:set,
+    get:get,
+    has:has,
+    remove:remove,
+    length:length,
+    clear:clear,
+    keys:keys,
+    values:values,
+    entries:entries,
+  }
+
+}
+
+const test = hashMap();
+
+test.set('frog');
+test.set('apple');
